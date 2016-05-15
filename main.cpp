121573d733c2b4f3dc65f1b933ef6ac107af8c4c
Luis Fernando Chacón <luisferchacon.lfc@gmail.com>
2016-05-15 17:19:20 -0500

    ProyectoInicial

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: fercris
+ *
+ * Created on 15 de mayo de 2016, 01:09 AM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
